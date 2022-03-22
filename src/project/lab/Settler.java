26da9bd159f439ad8e2a2414eff8cc51aada7b16
LUHAITIAN521 <luhaitian521@gmail.com>
2022-03-22 23:36:26 +0100

    Initial commit

--- src/project/lab/Settler.java
+++ src/project/lab/Settler.java
@@ -1,4 +1,10 @@
 package project.lab;
 
 public class Settler {
+<<<<<<< HEAD
+=======
+    public void Buildspace_station(){
+
+    }
+>>>>>>> 33d40d4 (Initial commit)
 }
--- src/project/lab/Sun.java
+++ src/project/lab/Sun.java
@@ -1,4 +1,8 @@
 package project.lab;
 
 public class Sun {
+    public void createstorm(){
+        Sun sun = new Sun();
+
+    }
 }
