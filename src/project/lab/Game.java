0edfd10a4e7af032d1d5c8ad058f9ab576313e51
PompeiiDark <55684364+PompeiiDark@users.noreply.github.com>
2022-03-25 05:49:27 +0800

    Update Game.java

--- src/project/lab/Game.java
+++ src/project/lab/Game.java
@@ -10,36 +10,47 @@ public class Game {
   public void enggame(){
   }
 
-  public void initial() throws IOException {
-    String enter;
-    UnitTest.methodTest("Game.Initial()");
-    System.out.println("please choose play mode");
-    System.out.println("1:SinglePlayer");
-    System.out.println("2.MultiplePlayer");
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    enter = br.readLine();
-    int num = Integer.parseInt(enter);
-    if(num==1){
-      SinglePlayer();
-    }
-
-    if(num==2){
-      MultiplePlayer();
-    }
-    else
-      System.out.println("Invalid choose");
-  }
-
-  private void MultiplePlayer() {
-    UnitTest.methodTest("Game.MultiplePlayer()");
-    Sun sun = new Sun();
-  }
-
-  private void SinglePlayer() {
-    UnitTest.methodTest("Game.SinglePlayer");
-    Sun sun = new Sun();
-  }
+  public void Initial() throws IOException
+	{
+		String enter;
+		UnitTest.methodTest("Game.Initial()");
+		System.out.println("Please choose play mode:");
+        System.out.println("1:SinglePlayer");
+        System.out.println("2.MultiplePlayer");
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        enter = br.readLine();
+        int num = Integer.parseInt(enter);
+        if(num==1)
+        {
+            SinglePlayer();
+        }
+        else if(num==2){
+            MultiplePlayer();
+        }
+        else 
+        System.out.println("Invaild choose");
+	}
+	public void SinglePlayer()
+	{
+		UnitTest.methodTest("Game.SinglePlayer()");
+		Sun s=new Sun();
+		Asteroids a=new Asteroids();
+		s.AddAsteroids();
+		a.CreateCore();
+		a.CreateResource();
+		UnitTest.TestEnd(" Settler.SinglePlayer()");
+	}
+	public void MultiplePlayer()
+	{
+		UnitTest.methodTest("Game.MultiplePlayer()");
+		Sun s=new Sun();
+		Asteroids a=new Asteroids();
+		s.AddAsteroids();
+		a.CreateCore();
+		a.CreateResource();
+		UnitTest.TestEnd(" Settler.MultiplePlayer()");
+	}
 
   public void InitialPosition(){
     Sun sun = new Sun();
